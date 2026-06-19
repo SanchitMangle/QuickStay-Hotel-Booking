bca5a1fd390f723c224aa00565ef84d96ab66b46
SanchitMangle <sanchitmangle12345@gmail.com>
2026-06-19 15:29:51 +0530

    home page completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclosiveOffers from '../components/ExclosiveOffers'
+import Testmoial from '../components/Testmoial'
 
 const Home = () => {
     return (
         <>
             <HeroSection />
+            <FeaturedDestination />
+            <ExclosiveOffers />
+            <Testmoial />
         </>
     )
 }
